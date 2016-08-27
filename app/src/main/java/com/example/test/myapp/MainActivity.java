b2365707d9ceed418206af350223c76b1b863746
mhy <susia7@gmail.com>
2016-08-28 00:20:28 +0900

    SecondCommit Test

--- app/src/main/java/com/example/test/myapp/MainActivity.java
+++ app/src/main/java/com/example/test/myapp/MainActivity.java
@@ -11,4 +11,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
